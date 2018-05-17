ff3e26f80255898b2fa2a0c1feaf9e825a4356e7
shankajarajapakse <sankaja.14@cse.mrt.ac.lk>
2018-05-17 11:02:59 -0700

    XML reader structure change

--- src/main/java/services/XMLReaderService.java
+++ src/main/java/services/XMLReaderService.java
@@ -1,6 +1,9 @@
 package services;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -12,24 +15,43 @@ import java.io.IOException;
 public class XMLReaderService
 {
     private XMLReaderService instance = null;
+    private NodeList nodeList;
 
-    private XMLReaderService() {}
+    private XMLReaderService() throws IOException, SAXException, ParserConfigurationException {
+        setNodeList();
+    }
 
-    public XMLReaderService getInstance() {
+    public XMLReaderService getInstance() throws ParserConfigurationException, SAXException, IOException {
         if (instance == null)
             instance = new XMLReaderService();
         return instance;
     }
 
-    public String getValue(String field, int valueIndex) throws ParserConfigurationException, IOException, SAXException {
+    private NodeList setNodeList() throws ParserConfigurationException, IOException, SAXException {
+
         String filePath = "C:\\Users\\Sankaja\\IdeaProjects\\Middleware\\resources\\properties.xml";
-        File file = new File("userdata.xml");
+        File file = new File(filePath);
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
                 .newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         Document document = documentBuilder.parse(file);
-        String value = document.getElementsByTagName(field).item(valueIndex).getTextContent();
 
-        return value;
+        NodeList nodeList = document.getElementsByTagName("attribute");
+
+        return nodeList;
+    }
+
+    public String[] getFields()
+    {
+        int fieldCount = this.nodeList.getLength();
+        String[] fields = new String[fieldCount];
+        Element e;
+
+        for (int i = 0 ; i <= fieldCount ; i++)
+        {
+            e = (Element) this.nodeList.item(i);
+            fields[i] = e.getAttribute("value");
+        }
+        return fields;
     }
 }
